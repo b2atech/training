6052892df807adf4a3bc23407df1d938b016af3a
Bharat <bharat.mane@gmail.com>
2022-11-12 11:57:17 +0530

    feat: add team member specific projects

---
+++ b2a.SK.LearnCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace b2a.SK.LearnCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
