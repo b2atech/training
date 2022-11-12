d07a68e8e0c9aafd0c4efd2de2d9df9c586303e6
Bharat <bharat.mane@gmail.com>
2022-11-12 11:42:47 +0530

    feat: add initial commit

---
+++ b2a.HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace b2a.HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to B2A Technologies!");
+            
+        }
+    }
+}
